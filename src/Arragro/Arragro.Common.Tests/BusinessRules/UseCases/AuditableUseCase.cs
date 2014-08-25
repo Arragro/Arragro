9eba2bf52d24ee5a40ddabc5b81b24b4a60e5ed4
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-08-25 12:09:01 +1200

    Added a Auditable object and an interface
    Related Work Items: #80

---
+++ src/Arragro/Arragro.Common.Tests/BusinessRules/UseCases/AuditableUseCase.cs
@@ -0,0 +1,39 @@
+﻿using Arragro.Common.BusinessRules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Arragro.Common.Tests.BusinessRules.UseCases
+{
+    public class AuditableUseCase
+    {
+        private class AuditableTestInt : IAuditable<int>
+        {
+            public int CreatedBy { get; set; }
+            public int ModifiedBy { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public DateTime ModifiedDate { get; set; }
+        }
+
+        private class EntityFoo : AuditableTestInt
+        {
+            public int EntityFooId { get; set; }
+        }
+
+        [Fact]
+        public void AuditableUseCaseTest()
+        {
+            var entityFoo = new EntityFoo
+            {
+                EntityFooId = 1,
+                CreatedBy = 1,
+                ModifiedBy = 1,
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now
+            };
+        }
+    }
+}
---
+++ src/Arragro/Arragro.Common/BusinessRules/Auditable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arragro.Common.BusinessRules
+{
+    public interface IAuditable<TUserIdType>
+    {
+        TUserIdType CreatedBy { get; set; }
+        TUserIdType ModifiedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime ModifiedDate { get; set; }
+    }
+
+    public class Auditable<TUserIdType> : IAuditable<TUserIdType>
+    {
+        public TUserIdType CreatedBy { get; set; }
+        public TUserIdType ModifiedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
