e38f57856a4fe8ffe76ff97013121f2a3e425b5c
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-09-14 16:58:34 +1200

    Added DbContextRepositoryBase
    Related Work Items: #119

---
+++ src/Arragro.EF6/DbContextRepositoryBase.cs
@@ -0,0 +1,84 @@
+﻿using Arragro.Common.Repository;
+using Arragro.EF6.Interfaces;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Arragro.EF6
+{
+    public class DbContextRepositoryBase<TEntity, TKey> : 
+        IDbContextRepositoryBase<TEntity, TKey>,
+        IRepository<TEntity, TKey> where TEntity : class
+    {
+        private readonly IBaseContext _baseContext;
+
+        public IBaseContext BaseContext { get { return _baseContext; } }
+
+        public DbContextRepositoryBase(IBaseContext baseContext)
+        {
+            _baseContext = baseContext;
+        }
+
+        protected IDbSet<TEntity> DbSet
+        {
+            get { return _baseContext.Set<TEntity>(); }
+        }
+
+        public void TurnOnOffLazyLoading(bool enabled = true)
+        {
+            _baseContext.Configuration.LazyLoadingEnabled = enabled;
+        }
+
+        public TEntity Find(TKey id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public TEntity Delete(TKey id)
+        {
+            return DbSet.Remove(Find(id));
+        }
+
+        public virtual IQueryable<TEntity> All()
+        {
+            return DbSet;
+        }
+
+        public virtual IQueryable<TEntity> All(Expression<Func<TEntity, bool>> whereClause)
+        {
+            return DbSet.Where(whereClause);
+        }
+
+        public TEntity InsertOrUpdate(TEntity model, bool add)
+        {
+            if (add)
+            {
+                return DbSet.Add(model);
+            }
+            else
+            {
+                _baseContext.SetModified(model);
+                return model;
+            }
+        }
+
+        public int SaveChanges()
+        {
+            try
+            {
+                return _baseContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                var innerEx = ex.InnerException;
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            _baseContext.Dispose();
+        }
+    }
+}
