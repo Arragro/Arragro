9501ac758aa1f06c8ebf53ce5d58d112840f0417
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-09-16 22:23:43 +1200

    Log4NetProvider
    Related Work Items: #96, #121, #122, #123, #124

---
+++ src/Arragro.Log4Net/CSV/EndRowConverter.cs
@@ -0,0 +1,17 @@
+﻿using log4net.Util;
+using System.IO;
+
+namespace Arragro.Log4Net.CSV
+{
+    public class EndRowConverter : PatternConverter
+    {
+        protected override void Convert(TextWriter writer, object state)
+        {
+            var ctw = writer as CsvTextWriter;
+            // write the ending quote for the last field
+            if (ctw != null)
+                ctw.WriteQuote();
+            writer.WriteLine();
+        }
+    }
+}
