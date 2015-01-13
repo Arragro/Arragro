346e4a401e7179b6aa12b11853652a6d7715b994
Michael Jewell <michael_jewell@hotmail.co.uk>
2015-01-13 20:47:03 +1300

    Added the ability to add a new Object History.

--- src/Arragro.ObjectLogging/Interfaces/IObjectHistoryService.cs
+++ src/Arragro.ObjectLogging/Interfaces/IObjectHistoryService.cs
@@ -8,6 +8,7 @@ namespace Arragro.ObjectLogging.Interfaces
 {
     public interface IObjectHistoryService
     {
+        ObjectHistory AddHistory<TModel, TKeyType>(TModel newObject, string userName);
         ObjectHistory AddHistory<TModel, TKeyType>(TModel originalObject, TModel newObject, string userName);
         IEnumerable<ObjectHistory> GetObjectHistories(Type type, string id);
     }
