71b0d696cd08f2b83d3855d6a912f8429209c796
Michael Jewell <michael_jewell@hotmail.co.uk>
2015-01-13 16:26:40 +1300

    Rnamed Arragro.ObjectLogging.MVC first stage.

---
+++ src/Arragro.ObjectLogging.MVC_Temp/Services/ObjectHistoryService.cs
@@ -0,0 +1,45 @@
+﻿using Arragro.Common.ServiceBase;
+using Arragro.ObjectLogging;
+using Arragro.ObjectLogging.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arragro.ObjectLogging.Mvc.Services
+{
+    public class ObjectHistoryService : Service<IObjectHistoryRepository, ObjectHistory, Guid>, IObjectHistoryService
+    {
+        public ObjectHistoryService(IObjectHistoryRepository objectHistoryRepository)
+            : base(objectHistoryRepository)
+        {
+        }
+
+        public IEnumerable<ObjectHistory> GetObjectHistories(Type type, string id)
+        {
+            var key = type.Name + ":" + id.ToString();
+            return Repository.All()
+                        .Where(o => o.Key == key)
+                        .OrderBy(o => o.DateChanged)
+                        .ToList();
+        }
+
+        public override ObjectHistory InsertOrUpdate(ObjectHistory objectHistory)
+        {
+            var add = objectHistory.ObjectHistoryId == default(Guid) ? true : false;
+            objectHistory.SetObjectObjectHistoryId();
+            return Repository.InsertOrUpdate(objectHistory, add);
+        }
+
+        protected override void ValidateModelRules(ObjectHistory objectHistory)
+        {
+            if (RulesException.Errors.Any()) throw RulesException;
+        }
+
+        public ObjectHistory AddHistory<TModel, TKeyType>(TModel originalObject, TModel newObject, string userName)
+        {
+            var objectHistory = InsertOrUpdate(ObjectHistoryExtensions.BuildObjectHistory<TModel, TKeyType>(originalObject, newObject, userName));
+            SaveChanges();
+            return objectHistory;
+        }
+    }
+}
