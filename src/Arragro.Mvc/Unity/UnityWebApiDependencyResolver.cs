ac592b7660c88ad7440e4e8f1e74cd657f8a5062
Michael Jewell <mike@arragro.com>
2015-01-23 17:00:07 +1300

    Added custom unity dependency resolver that does not require HttpContext.

---
+++ src/Arragro.Mvc/Unity/UnityWebApiDependencyResolver.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Dependencies;
+
+namespace Arragro.Mvc.Unity
+{
+    public class UnityWebApiDependencyResolver : IDependencyResolver, IDisposable
+    {
+        protected IUnityContainer container;
+        protected IDependencyResolver resolver;
+
+        public UnityWebApiDependencyResolver(IUnityContainer container, IDependencyResolver resolver)
+        {
+            this.container = container;
+            this.resolver = resolver;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = container.CreateChildContainer();
+            return new UnityWebApiDependencyResolver(child, resolver);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return this.container.Resolve(serviceType);
+            }
+            catch
+            {
+                return this.resolver.GetService(serviceType);
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return this.container.ResolveAll(serviceType);
+            }
+            catch
+            {
+                return this.resolver.GetServices(serviceType);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool dispose)
+        {
+            container.Dispose();
+        }
+    }
+}
