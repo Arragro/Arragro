0d4a82343d9f48a500c39ae9605a6c7771006103
Michael Jewell <mike@arragro.com>
2015-03-25 16:30:56 +1300

    Added Shared Access Signature functionality.

--- src/Arragro.Azure/ServiceManagement/BlobStorageManager.cs
+++ src/Arragro.Azure/ServiceManagement/BlobStorageManager.cs
@@ -30,7 +30,7 @@ namespace Arragro.Azure.ServiceManagement
         {
             return _cloudBlobClient.GetContainerReference(containerName);
         }
-
+        
         public CloudBlobContainer CreateBlobContainer(string containerName)
         {
             var container = GetBlobContainer(containerName);
@@ -38,9 +38,44 @@ namespace Arragro.Azure.ServiceManagement
             return container;
         }
 
+        public static string GetContainerSasUri(
+            CloudBlobContainer container,
+            string policyName,
+            int sharedAccessExpiryTimeInHours = 24,
+            SharedAccessBlobPermissions sharedAccessBlobPermissions = SharedAccessBlobPermissions.List)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            var sharedPolicy = new SharedAccessBlobPolicy();
+            sharedPolicy.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(sharedAccessExpiryTimeInHours);
+            sharedPolicy.Permissions = sharedAccessBlobPermissions;
+
+            //Get the container's existing permissions.
+            BlobContainerPermissions permissions = new BlobContainerPermissions();
+
+            //Add the new policy to the container's permissions.
+            permissions.SharedAccessPolicies.Clear();
+            permissions.SharedAccessPolicies.Add(policyName, sharedPolicy);
+            container.SetPermissions(permissions);
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            string sasContainerToken = container.GetSharedAccessSignature(null, policyName);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+
         public CloudBlockBlob UploadStream(string containerName, Stream stream, string blobName)
         {
             var container = GetBlobContainer(containerName);
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            blockBlob.UploadFromStream(stream);
+            return blockBlob;
+        }
+
+        public static CloudBlockBlob UploadStream(Uri uri, Stream stream, string blobName)
+        {
+            var container = new CloudBlobContainer(uri);
             var blockBlob = container.GetBlockBlobReference(blobName);
             blockBlob.UploadFromStream(stream);
             return blockBlob;
