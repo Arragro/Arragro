64af858159586d9971c9be81be0f6c8d92a53c7b
Michael Jewell <michael_jewell@hotmail.co.uk>
2015-05-20 12:29:41 +1200

    Added EF functionality to migrate and drop DB's

---
+++ src/Arragro.EF6/DatabaseMigrator.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace Arragro.EF6
+{
+    public static class DatabaseMigrator
+    {
+        public static void Migrate(DbMigrationsConfiguration dbMigrationConfiguration)
+        {
+            var migrator = new DbMigrator(dbMigrationConfiguration);
+            migrator.Configuration.AutomaticMigrationsEnabled = true;
+            migrator.Update();
+        }
+    }
+}
