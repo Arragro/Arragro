68822469196d3e3508e07f910c1c2216a3e43904
Michael Jewell <michael_jewell@hotmail.co.uk>
2015-09-11 14:33:57 +1200

    Made a change to the way Replay Request is validated

--- src/Arragro.WebApi/Filters/HMACAuthenticationAttribute.cs
+++ src/Arragro.WebApi/Filters/HMACAuthenticationAttribute.cs
@@ -177,10 +177,7 @@ namespace Arragro.WebApi.Filters
             DateTime epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
             TimeSpan currentTs = DateTime.UtcNow - epochStart;
 
-            var serverTotalSeconds = Convert.ToUInt64(currentTs.TotalSeconds);
-            var requestTotalSeconds = Convert.ToUInt64(requestTimeStamp);
-
-            if ((serverTotalSeconds - requestTotalSeconds) > _requestMaxAgeInSeconds)
+            if ((currentTs.TotalSeconds - long.Parse(requestTimeStamp)) > _requestMaxAgeInSeconds)
             {
                 return true;
             }
