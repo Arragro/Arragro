4c21f93bdbf90eee3f470eb367749e6a36f5af26
Michael Jewell <michael_jewell@hotmail.co.uk>
2016-03-30 10:30:07 +1300

    Moved exception models to allow for reduced overhead on package dependencies

---
+++ src/Arragro.Common/Exceptions/ApiErrorData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Arragro.Common.Exceptions
+{
+    public class ApiErrorData
+    {
+        public string Message { get; set; }
+        public Dictionary<string, IEnumerable<string>> ModelState { get; set; }
+
+        public ApiErrorData() { }
+    }
+}
---
+++ src/Arragro.Common/Exceptions/ApiHttpException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Arragro.Common.Exceptions
+{
+    public class ApiHttpException : Exception
+    {
+        public ApiHttpException(string message) : base(message) { }
+    }
+}
