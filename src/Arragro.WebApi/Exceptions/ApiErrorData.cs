fb2d7dcb0be88ed2326e8cf1ca841c810e681e8f
Michael Jewell <michael_jewell@hotmail.co.uk>
2016-03-30 09:57:34 +1300

    Added Exception models for api

---
+++ src/Arragro.WebApi/Exceptions/ApiErrorData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Arragro.WebApi.Exceptions
+{
+    public class ApiErrorData
+    {
+        public string Message { get; set; }
+        public Dictionary<string, IEnumerable<string>> ModelState { get; set; }
+
+        public ApiErrorData() { }
+    }
+}
---
+++ src/Arragro.WebApi/Exceptions/ApiHttpException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Arragro.WebApi.Exceptions
+{
+    public class ApiHttpException : Exception
+    {
+        public ApiHttpException(string message) : base(message) { }
+    }
+}
