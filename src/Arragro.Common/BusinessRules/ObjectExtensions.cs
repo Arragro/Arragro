cad9a16a8e263ec311c3a029c7d5dcff1654fdff
Michael Jewell <michael_jewell@hotmail.co.uk>
2017-02-24 15:46:16 +1300

    Added ObjectExtensions with a ValidateModelProperties method

---
+++ src/Arragro.Common/BusinessRules/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Arragro.Common.BusinessRules
+{
+    public static class ObjectExtensions
+    {
+        public static IList<ValidationResult> ValidateModelProperties<TModel>(this TModel model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, validationContext, validationResults, true);
+            return validationResults;
+        }
+    }
+}
